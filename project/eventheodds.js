d3840d1ea55fc89746175f8fae0f10e5abb691d8
Kimmi Sin <kimmisin84@gmail.com>
2021-04-01 18:32:57 -0500

    Doesn't have Braden's, has updated search bar

--- project/eventheodds.js
+++ project/eventheodds.js
@@ -1,6 +1,11 @@
 function eventheodds() {
 	// keep adding location page names here
-	locations = ['krause', 'mayfield', 'mckinney', 'waterloo', 'zilker'];
+	locations = ['krause', 'mayfield', 'mckinney', 'waterloo', 'zilker',
+	'bullock', 'blanton', 'weird', 'thinkery', 'capitol', 
+	'acl', 'sxsw', 'zach', 'long', '6th',
+	'pcl', 'monkey', 'medici', 'fac', 'union',
+	'homeslice', 'gus', 'gordoughs', 'patio', 'franklins',
+	'dkr', 'greg', 'circuit', 'fec', 'bouldering' ];
 
 	randomIdx = Math.trunc(Math.random()*locations.length);
 
---
+++ project/search.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title> Search Functionality </title>
+	<meta charset="UTF-8">
+	<meta name="description" content="PHP file for search bar functionality">
+	<meta author="name" content="Kimmi Sin">
+</head>
+
+<body>
+	<h1> Blank php page: </h1>	
+	<p> Not sure how to implement search bar functionality<br> Kimmi Sin | Last Updated: 04/01/2021</p>
+</body>
+</html>
